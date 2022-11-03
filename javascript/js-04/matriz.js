f735d09371cb2f5ee6c1eba83d97e146c3ba94fa
RichieVG <josericardovaldiviagonzalez@gmail.com>
2022-11-03 14:35:16 -0600

    Loop

---
+++ javascript/js-04/matriz.js
@@ -0,0 +1,35 @@
+let arr = [
+    
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+
+];
+
+let contador = 0;
+
+/* while(contador < 10 ){
+
+    console.log(contador);
+
+    contador = contador + 1;
+} */
+/* do{
+console.log(contador);
+contador ++;
+}while(contador <= 10); */
+
+/* let cadena = "Hola";
+console.log(cadena.length); */
+
+let fila = 0;
+while(fila < 3){
+
+    for (let columna = 0; columna <= 2; columna++){
+        
+        console.log(arr [fila][columna]);
+
+    }
+    fila ++;
+    console.log("-------------");
+}
