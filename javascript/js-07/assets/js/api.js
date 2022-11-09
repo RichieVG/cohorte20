22be1d6c9effa20c02287883c8a54816791377a2
RichieVG <josericardovaldiviagonzalez@gmail.com>
2022-11-09 17:37:48 -0600

    Pokedex

--- javascript/js-07/assets/js/api.js
+++ javascript/js-07/assets/js/api.js
@@ -17,28 +17,116 @@ foo();
 baz(); */
 
 
-const nombre = () => {
+/* const nombre = () => {
     const nombres = ["Pepe", "Pancho", "María"];
 
     setTimeout(() => {
         return nombres;
     }, 0);
-}
+} */
 
 //console.log(nombre()[0]);
 
 
-const url = "https://pokeapi.co/api/v2/pokemon/4";
+/* const url = "https://pokeapi.co/api/v2/pokemon/4";
 
 const pokedex = () => {
     fetch(url)
         .then((datos) => datos.json())
         .then((pokemon) => console.log(pokemon))
         .catch(error => console.log(error))
-};
+}; */
 
 
-pokedex();
+/* pokedex(); */
 
 // () => {return}
 
+/* const nombres = () => {
+    const nombres = ["Pepe", "Pancho", "María"]; */
+
+    
+
+    /* setTimeout(() => {
+        return nombres;
+    }, 0); */
+
+    /* return new Promise((resolve, reject) => { //resolve -- proceso exitoso
+        setTimeout(() => {
+                                //reject -- proceso no exitoso
+           if(true){
+            resolve(nombres);
+           }else{
+            reject("No hay nombres");
+           }
+           
+        }, 3000)
+    });
+ */
+/* }
+nombre().then(nombres => console.log(nombres)).catch(error => console.warn(error));
+
+const obtenerNombre = async () => {
+
+    try {
+        let nombres = await nombre();
+        console.log(nombres);
+
+    } catch(error) {
+        console.log(error);
+    }
+} */
+
+/* obtenerNombre(); */
+const cartaPokemon = document.getElementById('pokemon');
+const nombrePokemon = document.getElementById('nombre');
+const imagen = document.getElementById('img');
+
+
+const pokedex = async (numero) => {
+    try {
+
+        const url = `https://pokeapi.co/api/v2/pokemon/${numero}`;
+
+        const respuesta = await fetch(url);
+        const pokemon = await respuesta.json();
+        
+        console.log(pokemon.name);
+
+        const datos = {
+            nombre: pokemon.name,
+            imagen: pokemon.sprites.other['official-artwork'].front_default,
+        }
+
+        console.log(datos.imagen);
+
+        nombrePokemon.textContent = datos.nombre;
+        imagen.innerHTML = `
+            <img src="${datos.imagen}" alt="imagen de ${datos.nombre}" width="250"/>
+        `;
+        
+        
+    } catch (error) {
+
+
+        
+    }
+}
+
+
+
+form.addEventListener("submit", (evento) => {
+    //Codigo
+    evento.preventDefault();
+
+    const valor = document.getElementById("idP").value;
+
+    console.log(valor);
+
+
+    pokedex(valor);
+    //console.log(evento.target);
+
+    form.reset();
+
+});
