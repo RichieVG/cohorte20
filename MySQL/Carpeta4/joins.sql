64443de1bc1d15cdf41c0dffa8448dd0c61860bc
RichieVG <josericardovaldiviagonzalez@gmail.com>
2022-12-09 17:52:04 -0600

    09/12

--- MySQL/Carpeta4/joins.sql
+++ MySQL/Carpeta4/joins.sql
@@ -5,5 +5,36 @@ SELECT * FROM categoria;
 SELECT * FROM productos, categoria
 where idCategoria = id;
 
+/* INNER */
 SELECT p.nombre, p.precio, p.existencia, c.nombre FROM productos as p inner join categoria as c
 on p.idCategoria = c.id;
+
+/* LEFT JOIN */
+SELECT p.nombre, p.precio, p.existencia, c.nombre FROM productos as p left join temp as t
+on p.idCategoria = t.id;
+
+/* RIGHT JOIN  */
+SELECT p.nombre, p.precio, p.existencia, c.nombre FROM productos as p left join temp as t
+on p.idCategoria = t.id;
+
+SELECT * FROM proveedores;
+
+SELECT * FROM categoria union
+SELECT nombre, precio FROM productos;
+
+/* multi consultas */
+SELECT p.nombre, p. precio, p.existencia FROM productos as p;
+
+select idCategoria from productos;
+
+select * from proveedores;
+
+select p.nombre as nombre_producto, c.nombre as nombre_categoria, pv.nombre as nombre_proveedor
+from productos as p
+left join categoria as c
+on p.idCategoria = c.id
+left join detalle_producto_proveedor as detalle
+on p.idProducto = detalle.idProducto
+left join proveedores as pv
+on detalle.id = pv.id;
+
