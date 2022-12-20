71b3eb7b1e1d97a9879eabcea3f041711efac23c
RichieVG <josericardovaldiviagonzalez@gmail.com>
2022-12-20 07:31:27 -0600

    20/12

---
+++ java-web-1/ejemplo0-4jwt/src/main/java/com/generation/jrvg/myappjwt/controllers/PruebaController.java
@@ -0,0 +1,51 @@
+package com.generation.jrvg.myappjwt.controllers;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.slf4j.LoggerFactory;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import ch.qos.logback.classic.Logger;
+
+@RestController
+@RequestMapping("")
+public class PruebaController {
+	private static final Logger logger = (Logger)LoggerFactory.getLogger(PruebaController.class);
+	@GetMapping("/mensaje")
+	public ResponseEntity<?> getMensaje(){
+		var auth = SecurityContextHolder.getContext().getAuthentication();
+		logger.info("Datos usuario {}", auth.getPrincipal());
+		logger.info("Permisos usuario", auth.getAuthorities());
+		logger.info("Estado usuario", auth.isAuthenticated());
+		Map<String,String> mensaje = new HashMap<>();
+		mensaje.put("contenido", "Hola Mundo Jwt");
+		return ResponseEntity.ok(mensaje);
+	}
+	
+	@GetMapping("/publico")
+	public ResponseEntity<?> getMensajepublico(){
+		var auth = SecurityContextHolder.getContext().getAuthentication();
+		logger.info("Datos usuario {}", auth.getPrincipal());
+		logger.info("Permisos usuario", auth.getAuthorities());
+		logger.info("Estado usuario", auth.isAuthenticated());
+		Map<String,String> mensaje = new HashMap<>();
+		mensaje.put("contenido", "Hola Mundo publico querido Jwt");
+		return ResponseEntity.ok(mensaje);
+	}
+	
+	@GetMapping("/admin")
+	public ResponseEntity<?> getMensajeadmin(){
+		var auth = SecurityContextHolder.getContext().getAuthentication();
+		logger.info("Datos usuario {}", auth.getPrincipal());
+		logger.info("Permisos usuario", auth.getAuthorities());
+		logger.info("Estado usuario", auth.isAuthenticated());
+		Map<String,String> mensaje = new HashMap<>();
+		mensaje.put("contenido", "Hola querido admin");
+		return ResponseEntity.ok(mensaje);
+	}
+}
