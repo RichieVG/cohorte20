9f10df6c5d87939aeac205849293d17e37a32182
RichieVG <josericardovaldiviagonzalez@gmail.com>
2022-12-22 12:15:03 -0600

    REACT

--- reactProyect/my-app/src/MiComponente.js
+++ reactProyect/my-app/src/MiComponente.js
@@ -1,14 +1,40 @@
-import React, { Fragment } from "react"
-
-const  micomponente = () =>{
-    return
-    {/* <></> <Fragment></Fragment> <div></div>
-    <p>Este es mi primer componente</p> */}
-
-<>
-    <header>
-        <p>Este es mi primer componente</p>
-    </header>
-
-}
-export default micomponente
+import React from "react"; 
+ 
+const miComponente = () => { 
+ 
+  let nombre = "PedroVar"; 
+  let web = "Generation.org"; 
+  let usuario = { 
+    nombre: "JuanHer", 
+    apellido: "Robles", 
+    edad: 18 + " años" 
+  } 
+ 
+  return ( 
+    <> 
+    <header> 
+      <h1> Componente creado</h1> 
+      <ul> 
+        <li>° <em> <strong>Nombre:</strong> </em>  {JSON.stringify(usuario)}</li> 
+        <li>° <em> <strong>Web:</strong> </em> {web}</li> 
+        <li>° React</li> 
+        <li>° vue</li> 
+      </ul> 
+  <p> 
+    Este es mi primer componente 
+  </p> 
+  </header> 
+  </> 
+  ); 
+} 
+ 
+export default miComponente 
+ 
+ 
+{/* 
+  Esto es un comentario de bloque en React 
+*/} 
+ 
+/* 
+Esto es un comentario en React 
+*/
