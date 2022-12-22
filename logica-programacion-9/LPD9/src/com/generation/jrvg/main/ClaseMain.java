13fe827a27ea853b126193715db71db16fb0c5c6
RichieVG <josericardovaldiviagonzalez@gmail.com>
2022-12-22 12:03:07 -0600

    LPDs resueltos

--- logica-programacion-9/LPD9/src/com/generation/jrvg/main/ClaseMain.java
+++ logica-programacion-9/LPD9/src/com/generation/jrvg/main/ClaseMain.java
@@ -8,28 +8,16 @@ public class ClaseMain {
 	public static void main(String[] args) {
 		
 		HashMap<String, String> dicc = new HashMap<>();
+		
 		Scanner a = new Scanner(System.in);	
 		
-		dicc.put("reloj", "watch");
-		dicc.put("caliente", "hot");
-		dicc.put("frío", "cold");
-		dicc.put("tienda", "market");
-		dicc.put("cuchillo", "knife");
-		dicc.put("tenedor", "fork");
-		dicc.put("baño", "bathroom");
-		dicc.put("cabeza", "head");
-		dicc.put("riñón", "liver");
-		dicc.put("pie", "foot");
-		dicc.put("grande", "big");
-		dicc.put("pequeño", "little");
-		dicc.put("lento", "slow");
-		dicc.put("rápido", "fast");
-		dicc.put("celular", "cellphone");
-		dicc.put("plaza", "mall");
-		dicc.put("gato", "cat");
-		dicc.put("perro", "dog");
-		dicc.put("ratón", "mouse");
-		dicc.put("manos", "hands");
+		dicc.put("reloj", "watch"); dicc.put("caliente", "hot"); dicc.put("frío", "cold");
+		dicc.put("tienda", "market"); dicc.put("cuchillo", "knife"); dicc.put("tenedor", "fork");
+		dicc.put("baño", "bathroom"); dicc.put("cabeza", "head"); dicc.put("riñón", "liver");
+		dicc.put("pie", "foot"); dicc.put("grande", "big"); dicc.put("pequeño", "little");
+		dicc.put("lento", "slow"); dicc.put("rápido", "fast"); dicc.put("celular", "cellphone");
+		dicc.put("plaza", "mall"); dicc.put("gato", "cat"); dicc.put("perro", "dog");
+		dicc.put("ratón", "mouse");dicc.put("manos", "hands");
 		
 		String palabra;
 		System.out.println("Introduce la palabra bien escrita en español:");
