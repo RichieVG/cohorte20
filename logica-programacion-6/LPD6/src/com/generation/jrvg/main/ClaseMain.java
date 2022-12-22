be9dd38303ed5421723865406fafbcf5675b12f0
RichieVG <josericardovaldiviagonzalez@gmail.com>
2022-12-22 12:01:28 -0600

    LPDs resueltos

--- logica-programacion-6/LPD6/src/com/generation/jrvg/main/ClaseMain.java
+++ logica-programacion-6/LPD6/src/com/generation/jrvg/main/ClaseMain.java
@@ -13,8 +13,7 @@ public class ClaseMain {
 		System.out.println("Bienvenido a invertir el nombre!");
 		System.out.println("Introduce el nombre: ");
 		nombre=a.nextLine();
-		for (int i = 0; i < nombre.length(); i++) {
-			reverse=nombre.charAt(i)+reverse;
+		for (int i = 0; i < nombre.length(); i++) {reverse=nombre.charAt(i)+reverse;
 		}
 		System.out.println(reverse);
 		System.out.println("Este es tu nombre al revés!");
